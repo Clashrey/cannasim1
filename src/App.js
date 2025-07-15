4147a6e10e627052b60874c29c4268e2613dad3d
Clashrey <dan.podgurskiy@gmail.com>
2025-07-15 21:59:19 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CannabisSimulator from './components/CannabisSimulator';
+
+function App() {
+  return (
+    <div className="App">
+      <CannabisSimulator />
+    </div>
+  );
+}
+
+export default App;
